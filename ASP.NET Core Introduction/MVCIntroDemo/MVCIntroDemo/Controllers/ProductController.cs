d6ec2e3443d16184c0cbea83241fbe892c39a4fe
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-12-21 14:05:31 +0200

    Features

---
+++ ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using MVCIntroDemo.ViewModels.Product;
+using Newtonsoft.Json;
+using System.Text;
+using static MVCIntroDemo.Seeding.ProductData;
+
+namespace MVCIntroDemo.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult All(string keyword = "")
+		{
+			if (string.IsNullOrWhiteSpace(keyword))
+			{
+				return View(Products);
+			}
+
+			IEnumerable<ProductViewModel> productsAfterSearch = Products
+				.Where(p => p.Name.ToLower().Contains(keyword.ToLower()))
+				.ToArray();
+			return View(productsAfterSearch);
+		}
+
+		public IActionResult ById(string id)
+		{
+			ProductViewModel? product = Products
+				.FirstOrDefault(p => p.Id.ToString().Equals(id));
+
+			if (product == null)
+			{
+				return RedirectToAction("All");
+			}
+
+			return View(product);
+		}
+
+		public ActionResult AllAsJson() 
+		{
+			string jsonText = JsonConvert.SerializeObject(Products, Formatting.Indented);
+			return Json(jsonText);
+		}
+
+		public ActionResult DownloadProductsInfo()
+		{
+			StringBuilder sb = new();
+
+			foreach (var product in Products)
+			{
+				sb
+					.AppendLine($"Product with Id: {product.Id}")
+					.AppendLine($"## Product Name: {product.Name}")
+					.AppendLine($"## Price Price: {product.Price:f2}")
+					.AppendLine("-------------------------------------");
+			}
+
+			Response.Headers.Add(HeaderNames.ContentDisposition, "attachment;filename=products.txt");
+
+			return File(Encoding.UTF8.GetBytes(sb.ToString()), "text/plain");
+		}
+	}
+}
