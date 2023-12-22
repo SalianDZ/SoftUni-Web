3664c69035fb11e19275c7974f8480e71ef492a3
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-12-22 11:32:24 +0200

    Text Splitter App

---
+++ ASP.NET Core Introduction/Text Splitter App/Text Splitter App/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Text_Splitter_App.Models;
+using Text_Splitter_App.Models.Text;
+
+namespace Text_Splitter_App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextViewModel viewModel) 
+        {
+            if (String.IsNullOrWhiteSpace(viewModel.TextToSplit)) 
+            {
+                return RedirectToAction("Index", new TextViewModel()
+                { 
+                    SplitText = String.Empty,
+                    TextToSplit = String.Empty
+                });
+            }
+
+            string[] words = viewModel.TextToSplit.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            string splitText = String.Join(Environment.NewLine, words);
+
+            viewModel.SplitText = splitText;
+            return RedirectToAction("Index", viewModel);
+        }
+    }
+}
---
+++ ASP.NET Core Introduction/Text Splitter App/Text Splitter App/Models/Text/TextViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Text_Splitter_App.Models.Text
+{
+    public class TextViewModel
+    {
+        public string? TextToSplit { get; set; }
+
+        public string? SplitText { get; set; }
+    }
+}
