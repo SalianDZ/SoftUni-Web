e195d4b4b793cf387ecebaa51fde3258c46691b9
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-12-28 12:16:39 +0200

    Initial Commit

---
+++ ASP.NET and Databases/TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskBoardApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public HomeController()
+		{
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
