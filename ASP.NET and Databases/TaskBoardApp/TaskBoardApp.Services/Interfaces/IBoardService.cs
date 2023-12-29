698eb6d13f0743a8e06a486d4ec3ae418dd0d285
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-12-29 22:42:26 +0200

    Services

--- ASP.NET and Databases/TaskBoardApp/TaskBoardApp.Services/Interfaces/IBoardService.cs
+++ ASP.NET and Databases/TaskBoardApp/TaskBoardApp.Services/Interfaces/IBoardService.cs
@@ -1,9 +1,15 @@
 ﻿using TaskBoardApp.Web.ViewModels.Board;
+using TaskBoardApp.Web.ViewModels.Task;
 
 namespace TaskBoardApp.Services.Interfaces
 {
     public interface IBoardService
     {
         Task<IEnumerable<BoardAllViewModel>> AllAsync();
+
+        //Not implemented yet.
+		Task<BoardAllViewModel> AddAsync();
+
+		Task<BoardAllViewModel> AddAsync(TaskViewModel model);
     }
 }
