c3c9290a14fc487bb1c9025c69898cb14f436bd6
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2024-02-15 12:57:08 +0200

    Added entities, added indentity requirements, updated database with initial migration

---
+++ Web Fundamentals/Exam Preparation/Homies V2/Homies_Sketelon/Homies/Data/Models/Type.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Homies.Data.ValidationConstants.TypeValidations;
+
+namespace Homies.Data.Models
+{
+	public class Type
+	{
+		[Key]
+        public int Id { get; set; }
+
+		[Required]
+		[MaxLength(NameMaxLength)]
+		public string Name { get; set; } = null!;
+
+		public ICollection<Event> Events { get; set; } = new HashSet<Event>();
+    }
+}
---
+++ Web Fundamentals/Exam Preparation/Homies V2/Homies_Sketelon/Homies/Data/ValidationConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace Homies.Data
+{
+	public static class ValidationConstants
+	{
+		public static class EventValidations
+		{
+			public const int NameMinLength = 5;
+			public const int NameMaxLength = 20;
+
+			public const int DescriptionMinLength = 15;
+			public const int DescriptionMaxLength = 150;
+
+			public const string DateFormat = "yyyy-MM-dd H:mm";
+		}
+
+		public static class TypeValidations
+		{
+			public const int NameMinLength = 5;
+			public const int NameMaxLength = 15;
+		}
+	}
+}
